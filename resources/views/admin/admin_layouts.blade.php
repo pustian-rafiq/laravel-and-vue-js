6dadc860c5d7bb173a257514418d82cb0aeb8540
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2021-02-04 07:31:25 +0600

    admin panel set up

---
+++ resources/views/admin/admin_layouts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   @include('admin.partial.head');
+  </head>
+
+  <body>
+
+    <!-- ########## START: LEFT PANEL ########## -->
+  @include('admin.partial.sidebar');
+    <!-- ########## END: LEFT PANEL ########## -->
+
+    <!-- ########## START: HEAD PANEL ########## -->
+    @include('admin.partial.header');
+    <!-- ########## END: HEAD PANEL ########## -->
+ 
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    @yield('admin_content')
+     @include('admin.partial.footer');
+
+  </body>
+</html>
